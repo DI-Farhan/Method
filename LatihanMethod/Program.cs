96217c895acb46c30292ff5444a98ca022533b7c
DI-Farhan <pegasus998877@gmail.com>
2019-04-02 14:40:12 +0700

    Method Training

---
+++ LatihanMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+            Calculator calculator = new Calculator();
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil penjumlah: " + hasil);
+
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil penjumlah overload " + hasil);
+
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil penjumlah overload " + hasil);
+
+            hasil = calculator.Perkalian(5, 2);
+            CetakHasil("Hasil penjumlah ovserload " + hasil);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
